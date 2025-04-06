f9224ce6b57b295bebb2b25c5b96e3d5821eb2ee
arjay <jaywiz71@gmail.com>
2025-04-07 01:47:54 +0800

    Point mysql.createpool credentials to ENV file for db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,13 @@
 import mysql from "mysql2/promise";
+import dotenv from 'dotenv';
 
+dotenv.config();
 // Create MySQL Connection Pool
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "db_brighteeats",
+  host: process.env.HOST,
+  user:  process.env.USER,
+  password:  process.env.PW,
+  database:  process.env.DB,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
